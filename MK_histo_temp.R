e480d722ee056c7e81812b636618fd2195022e0a
Angela Cuervo <angelacuervo@iMac-de-Angela.local>
2018-09-06 18:01:08 -0500

    nada

--- MK_histo_temp.R
+++ MK_histo_temp.R
@@ -7,28 +7,28 @@ names(anps_todas)
 anp_coor <- anps_todas[c("POINT_X", "POINT_Y")]
 
 #
-MK_pvalue<-raster("TMaximaCNRMCM5_rcp85_MK_tau.tif")
+MK_pvalue<-raster("TpromedioCNRMCM5_rcp85_MK_tau.tif")
 names(MK_pvalue)<-"MK_p"
 Mk_p.df.CNRMCM5<-data.frame(extract(MK_pvalue,anp_coor), MGC="CNRMCM5_rcp85")
 names(Mk_p.df.CNRMCM5)[1]<-"Tendencia"
 Mk_CNRMCM5<-cbind(Mk_p.df.CNRMCM5,anps_todas)
 rm(MK_pvalue)
 
-MK_pvalue<-raster("TMaximaGFDL_CM3_rcp85_MK_tau.tif")
+MK_pvalue<-raster("TpromedioGFDL_CM3_rcp85_MK_tau.tif")
 names(MK_pvalue)<-"MK_p"
 Mk_p.df.GFDL_CM3<-data.frame(extract(MK_pvalue,anp_coor), MGC="GFDL_CM3_rcp85")
 names(Mk_p.df.GFDL_CM3)[1]<-"Tendencia"
 Mk_GFDL_CM3<-cbind(Mk_p.df.GFDL_CM3,anps_todas)
 rm(MK_pvalue)
 
-MK_pvalue<-raster("TMaximaHADGEM2_ES_rcp85_MK_tau.tif")
+MK_pvalue<-raster("TpromedioHADGEM2_ES_rcp85_MK_tau.tif")
 names(MK_pvalue)<-"MK_p"
 Mk_p.df.HADGEM2_ES<-data.frame(extract(MK_pvalue,anp_coor), MGC="HADGEM2_ES_rcp85")
 names(Mk_p.df.HADGEM2_ES)[1]<-"Tendencia"
 Mk_HADGEM2_ES<-cbind(Mk_p.df.HADGEM2_ES,anps_todas)
 rm(MK_pvalue)
 
-MK_pvalue<-raster("TMaximaMPI_ESM_LR_rcp85_MK_tau.tif")
+MK_pvalue<-raster("TpromedioMPI_ESM_LR_rcp85_MK_tau.tif")
 names(MK_pvalue)<-"MK_p"
 Mk_p.df.MPI_ESM_LR<-data.frame(extract(MK_pvalue,anp_coor), MGC="MPI_ESM_LR_rcp85")
 names(Mk_p.df.MPI_ESM_LR)[1]<-"Tendencia"
@@ -44,7 +44,7 @@ ggplot(PC_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/PC_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/PC_rcp85.jpg", dpi = 300)
 
 LT_rcp85<-subset(base,NOMBRE=="Laguna de Términos")           #2
 ggplot(LT_rcp85) +
@@ -52,7 +52,7 @@ ggplot(LT_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/LT_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/LT_rcp85.jpg", dpi = 300)
 
 TC_rcp85<-subset(base,NOMBRE=="Tehuacán-Cuicatlán")           #3
 ggplot(TC_rcp85) +
@@ -60,7 +60,7 @@ ggplot(TC_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/TC_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/TC_rcp85.jpg", dpi = 300)
 
 MM_rcp85<-subset(base,NOMBRE=="Mariposa Monarca")             #4
 ggplot(MM_rcp85) +
@@ -68,7 +68,7 @@ ggplot(MM_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/MM_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/MM_rcp85.jpg", dpi = 300)
 
 MN_rcp85<-subset(base,NOMBRE=="Manglares de Nichupté")        #5
 ggplot(MN_rcp85) +
@@ -76,7 +76,7 @@ ggplot(MN_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/MN_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/MN_rcp85.jpg", dpi = 300)
 
 J_rcp85<-subset(base,NOMBRE=="Janos")                         #6  
 ggplot(J_rcp85) +
@@ -84,7 +84,7 @@ ggplot(J_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/J_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/J_rcp85.jpg", dpi = 300)
 
 SSPM_rcp85<-subset(base,NOMBRE=="Sierra de San Pedro Mártir") #7
 ggplot(SSPM_rcp85) +
@@ -92,7 +92,7 @@ ggplot(SSPM_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/SSPM_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/SSPM_rcp85.jpg", dpi = 300)
 
 C1857_rcp85<-subset(base,NOMBRE=="Constitución de 1857")      #9
 ggplot(C1857_rcp85) +
@@ -100,7 +100,7 @@ ggplot(C1857_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/C1857_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/C1857_rcp85.jpg", dpi = 300)
 
 CS_rcp85<-subset(base,str_detect(base$NOMBRE,"Sumidero"))     #11
 ggplot(CS_rcp85) +
@@ -108,7 +108,7 @@ ggplot(CS_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/CS_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/CS_rcp85.jpg", dpi = 300)
 
 SO_rcp85<-subset(base,NOMBRE=="Selva El Ocote")               #12
 ggplot(SO_rcp85) +
@@ -116,7 +116,7 @@ ggplot(SO_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/SO_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/SO_rcp85.jpg", dpi = 300)
 
 EV_rcp85<-subset(base, str_detect(base$NOMBRE, "El Viz"))     #13 
 ggplot(EV_rcp85) +
@@ -124,7 +124,7 @@ ggplot(EV_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/EV_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/EV_rcp85.jpg", dpi = 300)
 
 CAD_rcp85<-subset(base,str_detect(base$NOMBRE,"C.A.D.N.R. 004 Don Mart"))   #8
 ggplot(CAD_rcp85) +
@@ -132,7 +132,7 @@ ggplot(CAD_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/CAD_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/CAD_rcp85.jpg", dpi = 300)
 
 M_rcp85<-subset(base, str_detect(base$NOMBRE, "Mapim"))  #10 
 ggplot(M_rcp85) +
@@ -140,6 +140,6 @@ ggplot(M_rcp85) +
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("tmax_graficas/M_rcp85.jpg", dpi = 300)
+ggsave("tmean_graficas/M_rcp85.jpg", dpi = 300)
 
 
