01c044b68d39d20557a89027f26f2681166f0514
Angela Cuervo <angelacuervo@iMac-de-Angela.local>
2018-09-06 12:22:09 -0500

    plots

--- MK_histo.R
+++ MK_histo.R
@@ -1,59 +1,147 @@
 library(ggplot2)
 library(stringr)
+library(raster)
 
+#setwd("/Volumes/GoogleDrive/Mi unidad/ANP_CC")
 anps_todas<-read.csv("anps_fed_terrestres2018.csv")
 names(anps_todas)
 anp_coor <- anps_todas[c("POINT_X", "POINT_Y")]
+#setwd("/Volumes/GoogleDrive/Mi unidad/MannKendall")
 
 #
-MK_pvalue<-raster("PrecipitacionCNRMCM5_rcp45_MK_tau_0_05.tif")
+MK_pvalue<-raster("PrecipitacionCNRMCM5_rcp85_MK_tau_0_05.tif")
 names(MK_pvalue)<-"MK_p"
-Mk_p.df.CNRMCM5<-data.frame(extract(MK_pvalue,anp_coor), MGC="CNRMCM5_rcp45")
+Mk_p.df.CNRMCM5<-data.frame(extract(MK_pvalue,anp_coor), MGC="CNRMCM5_rcp85")
 names(Mk_p.df.CNRMCM5)[1]<-"Tendencia"
 Mk_CNRMCM5<-cbind(Mk_p.df.CNRMCM5,anps_todas)
 rm(MK_pvalue)
 
-MK_pvalue<-raster("PrecipitacionGFDL_CM3_rcp45_MK_tau_0_05.tif")
+MK_pvalue<-raster("PrecipitacionGFDL_CM3_rcp85_MK_tau_0_05.tif")
 names(MK_pvalue)<-"MK_p"
-Mk_p.df.GFDL_CM3<-data.frame(extract(MK_pvalue,anp_coor), MGC="GFDL_CM3_rcp45")
+Mk_p.df.GFDL_CM3<-data.frame(extract(MK_pvalue,anp_coor), MGC="GFDL_CM3_rcp85")
 names(Mk_p.df.GFDL_CM3)[1]<-"Tendencia"
 Mk_GFDL_CM3<-cbind(Mk_p.df.GFDL_CM3,anps_todas)
 rm(MK_pvalue)
 
-MK_pvalue<-raster("PrecipitacionHADGEM2_ES_rcp45_MK_tau_0_05.tif")
+MK_pvalue<-raster("PrecipitacionHADGEM2_ES_rcp85_MK_tau_0_05.tif")
 names(MK_pvalue)<-"MK_p"
-Mk_p.df.HADGEM2_ES<-data.frame(extract(MK_pvalue,anp_coor), MGC="HADGEM2_ES_rcp45")
+Mk_p.df.HADGEM2_ES<-data.frame(extract(MK_pvalue,anp_coor), MGC="HADGEM2_ES_rcp85")
 names(Mk_p.df.HADGEM2_ES)[1]<-"Tendencia"
 Mk_HADGEM2_ES<-cbind(Mk_p.df.HADGEM2_ES,anps_todas)
 rm(MK_pvalue)
 
-MK_pvalue<-raster("PrecipitacionMPI_ESM_LR_rcp45_MK_tau_0_05.tif")
+MK_pvalue<-raster("PrecipitacionMPI_ESM_LR_rcp85_MK_tau_0_05.tif")
 names(MK_pvalue)<-"MK_p"
-Mk_p.df.MPI_ESM_LR<-data.frame(extract(MK_pvalue,anp_coor), MGC="MPI_ESM_LR_rcp45")
+Mk_p.df.MPI_ESM_LR<-data.frame(extract(MK_pvalue,anp_coor), MGC="MPI_ESM_LR_rcp85")
 names(Mk_p.df.MPI_ESM_LR)[1]<-"Tendencia"
 Mk_MPI_ESM_LR<-cbind(Mk_p.df.MPI_ESM_LR,anps_todas)
 rm(MK_pvalue)
 
 base<-rbind(Mk_CNRMCM5,Mk_GFDL_CM3, Mk_HADGEM2_ES,Mk_MPI_ESM_LR)
 
+##PLOTS
 PC_rcp85<-subset(base,NOMBRE=="Pantanos de Centla")           #1
-LT_rcp85<-subset(base,NOMBRE=="Laguna de Términos")           #2
-TC_rcp85<-subset(base,NOMBRE=="Tehuacán-Cuicatlán")           #3
-MM_rcp85<-subset(base,NOMBRE=="Mariposa Monarca")             #4
-MN_rcp85<-subset(base,NOMBRE=="Manglares de Nichupté")        #5
-J_rcp85<-subset(base,NOMBRE=="Janos")                         #6  
-SSPM_rcp85<-subset(base,NOMBRE=="Sierra de San Pedro Mártir") #7
-C1857_rcp85<-subset(base,NOMBRE=="Constitución de 1857")      #9
-CS_rcp85<-subset(base,str_detect(base$NOMBRE,"Sumidero"))     #11
-SO_rcp85<-subset(base,NOMBRE=="Selva El Ocote")               #12
-EV_rcp85<-subset(base, str_detect(base$NOMBRE, "El Viz"))     #13 
-CAD_rcp85<-subset(base,str_detect(base$NOMBRE,"C.A.D.N.R. 004 Don Mart"))   #8
-M_rcp85<-subset(base, str_detect(base$NOMBRE, "Mapim"))  #10 
-
-ggplot(CAD_rcp45) +
+ggplot(PC_rcp85) +
   geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
-ggsave("ppt_graficas/CAD_rcp45.jpg", dpi = 300)
+ggsave("ppt_graficas/PC_rcp85.jpg", dpi = 300)
+
+LT_rcp85<-subset(base,NOMBRE=="Laguna de Términos")           #2
+ggplot(LT_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/LT_rcp85.jpg", dpi = 300)
+
+TC_rcp85<-subset(base,NOMBRE=="Tehuacán-Cuicatlán")           #3
+ggplot(TC_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/TC_rcp85.jpg", dpi = 300)
+
+MM_rcp85<-subset(base,NOMBRE=="Mariposa Monarca")             #4
+ggplot(MM_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/MM_rcp85.jpg", dpi = 300)
+
+MN_rcp85<-subset(base,NOMBRE=="Manglares de Nichupté")        #5
+ggplot(MN_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/MN_rcp85.jpg", dpi = 300)
+
+J_rcp85<-subset(base,NOMBRE=="Janos")                         #6  
+ggplot(J_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/J_rcp85.jpg", dpi = 300)
+
+SSPM_rcp85<-subset(base,NOMBRE=="Sierra de San Pedro Mártir") #7
+ggplot(SSPM_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/SSPM_rcp85.jpg", dpi = 300)
+
+C1857_rcp85<-subset(base,NOMBRE=="Constitución de 1857")      #9
+ggplot(C1857_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/C1857_rcp85.jpg", dpi = 300)
+
+CS_rcp85<-subset(base,str_detect(base$NOMBRE,"Sumidero"))     #11
+ggplot(CS_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/CS_rcp85.jpg", dpi = 300)
+
+SO_rcp85<-subset(base,NOMBRE=="Selva El Ocote")               #12
+ggplot(SO_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/SO_rcp85.jpg", dpi = 300)
+
+EV_rcp85<-subset(base, str_detect(base$NOMBRE, "El Viz"))     #13 
+ggplot(EV_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/EV_rcp85.jpg", dpi = 300)
+
+CAD_rcp85<-subset(base,str_detect(base$NOMBRE,"C.A.D.N.R. 004 Don Mart"))   #8
+ggplot(CAD_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/CAD_rcp85.jpg", dpi = 300)
+
+M_rcp85<-subset(base, str_detect(base$NOMBRE, "Mapim"))  #10 
+ggplot(M_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("ppt_graficas/M_rcp85.jpg", dpi = 300)
+
 
--- MK_histo_temp.R
+++ MK_histo_temp.R
@@ -37,24 +37,109 @@ rm(MK_pvalue)
 
 base<-rbind(Mk_CNRMCM5,Mk_GFDL_CM3, Mk_HADGEM2_ES,Mk_MPI_ESM_LR)
 
+##PLOTS
 PC_rcp85<-subset(base,NOMBRE=="Pantanos de Centla")           #1
-LT_rcp85<-subset(base,NOMBRE=="Laguna de Términos")           #2
-TC_rcp85<-subset(base,NOMBRE=="Tehuacán-Cuicatlán")           #3
-MM_rcp85<-subset(base,NOMBRE=="Mariposa Monarca")             #4
-MN_rcp85<-subset(base,NOMBRE=="Manglares de Nichupté")        #5
-J_rcp85<-subset(base,NOMBRE=="Janos")                         #6  
-SSPM_rcp85<-subset(base,NOMBRE=="Sierra de San Pedro Mártir") #7
-C1857_rcp85<-subset(base,NOMBRE=="Constitución de 1857")      #9
-CS_rcp85<-subset(base,str_detect(base$NOMBRE,"Sumidero"))     #11
-SO_rcp85<-subset(base,NOMBRE=="Selva El Ocote")               #12
-EV_rcp85<-subset(base, str_detect(base$NOMBRE, "El Viz"))     #13 
-CAD_rcp85<-subset(base,str_detect(base$NOMBRE,"C.A.D.N.R. 004 Don Mart"))   #8
-M_rcp85<-subset(base, str_detect(base$NOMBRE, "Mapim"))  #10 
-
 ggplot(PC_rcp85) +
   geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
                  colour="grey50", alpha=0.5, position="identity")+
   facet_grid(MGC~.)
 
 ggsave("tmax_graficas/PC_rcp85.jpg", dpi = 300)
+
+LT_rcp85<-subset(base,NOMBRE=="Laguna de Términos")           #2
+ggplot(LT_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/LT_rcp85.jpg", dpi = 300)
+
+TC_rcp85<-subset(base,NOMBRE=="Tehuacán-Cuicatlán")           #3
+ggplot(TC_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/TC_rcp85.jpg", dpi = 300)
+
+MM_rcp85<-subset(base,NOMBRE=="Mariposa Monarca")             #4
+ggplot(MM_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/MM_rcp85.jpg", dpi = 300)
+
+MN_rcp85<-subset(base,NOMBRE=="Manglares de Nichupté")        #5
+ggplot(MN_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/MN_rcp85.jpg", dpi = 300)
+
+J_rcp85<-subset(base,NOMBRE=="Janos")                         #6  
+ggplot(J_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/J_rcp85.jpg", dpi = 300)
+
+SSPM_rcp85<-subset(base,NOMBRE=="Sierra de San Pedro Mártir") #7
+ggplot(SSPM_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/SSPM_rcp85.jpg", dpi = 300)
+
+C1857_rcp85<-subset(base,NOMBRE=="Constitución de 1857")      #9
+ggplot(C1857_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/C1857_rcp85.jpg", dpi = 300)
+
+CS_rcp85<-subset(base,str_detect(base$NOMBRE,"Sumidero"))     #11
+ggplot(CS_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/CS_rcp85.jpg", dpi = 300)
+
+SO_rcp85<-subset(base,NOMBRE=="Selva El Ocote")               #12
+ggplot(SO_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/SO_rcp85.jpg", dpi = 300)
+
+EV_rcp85<-subset(base, str_detect(base$NOMBRE, "El Viz"))     #13 
+ggplot(EV_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/EV_rcp85.jpg", dpi = 300)
+
+CAD_rcp85<-subset(base,str_detect(base$NOMBRE,"C.A.D.N.R. 004 Don Mart"))   #8
+ggplot(CAD_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/CAD_rcp85.jpg", dpi = 300)
+
+M_rcp85<-subset(base, str_detect(base$NOMBRE, "Mapim"))  #10 
+ggplot(M_rcp85) +
+  geom_histogram(aes(x=Tendencia, fill=MGC), bins = 40, 
+                 colour="grey50", alpha=0.5, position="identity")+
+  facet_grid(MGC~.)
+
+ggsave("tmax_graficas/M_rcp85.jpg", dpi = 300)
+
 
