bae68c2dcb585b25e44d0e6cd832e688dd181e6e
Angela Cuervo <angelacuervo@iMac-de-Angela.local>
2018-09-12 10:37:27 -0500

    manke

--- Base_Grafica_MannKendall_min.R
+++ Base_Grafica_MannKendall_min.R
@@ -10,144 +10,143 @@ names(anps_todas)
 anp_coor <- anps_todas[c("POINT_X", "POINT_Y")]
 
 #####CNRMCM5_####
-mk_pvalue<-raster("data_mk/tminimacnrmcm5_rcp85_mk_p.tif")
+mk_pvalue<-raster("data_mk/tminimacnrmcm5_rcp45_mk_p.tif")
 names(mk_pvalue)<-"mk_p"
-mk_tau<-raster("data_mk/tminimacnrmcm5_rcp85_mk_tau.tif")
+mk_tau<-raster("data_mk/tminimacnrmcm5_rcp45_mk_tau.tif")
 names(mk_tau)<-"mk_tau"
-mk_z<-raster("data_mk/tminimacnrmcm5_rcp85_mk_Z.tif")
+mk_z<-raster("data_mk/tminimacnrmcm5_rcp45_mk_Z.tif")
 names(mk_z)<-"mk_z"
 
-rcp85.p<-data.frame(extract(mk_pvalue,anp_coor), Variable="Pvalue")
-names(rcp85.p)[1]<-"Valor"
-rcp85.tau<-data.frame(extract(mk_tau,anp_coor), Variable="tau")
-names(rcp85.tau)[1]<-"Valor"
-rcp85.z<-data.frame(extract(mk_z,anp_coor), Variable="z")
-names(rcp85.z)[1]<-"Valor"
+rcp45.p<-data.frame(extract(mk_pvalue,anp_coor), Variable="Pvalue")
+names(rcp45.p)[1]<-"Valor"
+rcp45.tau<-data.frame(extract(mk_tau,anp_coor), Variable="tau")
+names(rcp45.tau)[1]<-"Valor"
+rcp45.z<-data.frame(extract(mk_z,anp_coor), Variable="z")
+names(rcp45.z)[1]<-"Valor"
 
-rcp85<-rbind(rcp85.tau, rcp85.p,rcp85.z)
-names(rcp85)
-head(rcp85)
+rcp45<-rbind(rcp45.tau, rcp45.p,rcp45.z)
+names(rcp45)
+head(rcp45)
 
-base.df<-cbind(rcp85, anps_todas)
+base.df<-cbind(rcp45, anps_todas)
 head(base.df)
 
 tb_vars <- base.df %>% split(.$Variable) %>%  
-  map(~ group_by(.x,.$NOMBRE) %>% summarise(mu=mean(complete.cases(Valor)), 
-                                                var= var(complete.cases(Valor)),
-                                                area =unique(Area_km),
-                                                area_log = log(unique(Area_km)))) %>%
-  map(~na.omit(.x)) %>% map(~ mutate(.x,variable_min=mu-var,
-                                     variable_max=mu+var,ID_anp=1:length(mu)))
+  map(~ group_by(.x,.$NOMBRE) %>% summarise(Promedio=mean(Valor), 
+                                                var= var(Valor),
+                                                Área =unique(Area_km),
+                                                Área_log = log(unique(Area_km)))) %>%
+  map(~na.omit(.x)) %>% map(~ mutate(.x,variable_min=Promedio-var,
+                                     variable_max=Promedio+var,ID_anp=1:length(Promedio)))
 
-write.csv(tb_vars,"tminimacnrmcm5_all_rcp85.csv")
+write.csv(tb_vars,"tminimacnrmcm5_all_rcp45.csv")
 
-CNRMCM5_rcp85.vars<-data.frame(read.csv("tminimacnrmcm5_all_rcp85.csv"), MGC="CNRMCM5_rcp85")
-names(CNRMCM5_rcp85.vars)
+CNRMCM5_rcp45.vars<-data.frame(read.csv("tminimacnrmcm5_all_rcp45.csv"), MGC="CNRMCM5 RCP 4.5")
+names(CNRMCM5_rcp45.vars)
 
 #####hadgem2_es_####
-mk_pvalue<-raster("data_mk/tminimahadgem2_es_rcp85_mk_p.tif")
+mk_pvalue<-raster("data_mk/tminimahadgem2_es_rcp45_mk_p.tif")
 names(mk_pvalue)<-"mk_p"
-mk_tau<-raster("data_mk/tminimahadgem2_es_rcp85_mk_tau.tif")
+mk_tau<-raster("data_mk/tminimahadgem2_es_rcp45_mk_tau.tif")
 names(mk_tau)<-"mk_tau"
-mk_z<-raster("data_mk/tminimahadgem2_es_rcp85_mk_Z.tif")
+mk_z<-raster("data_mk/tminimahadgem2_es_rcp45_mk_Z.tif")
 names(mk_z)<-"mk_z"
 
-rcp85.p<-data.frame(extract(mk_pvalue,anp_coor), Variable="Pvalue")
-names(rcp85.p)[1]<-"Valor"
-rcp85.tau<-data.frame(extract(mk_tau,anp_coor), Variable="tau")
-names(rcp85.tau)[1]<-"Valor"
-rcp85.z<-data.frame(extract(mk_z,anp_coor), Variable="z")
-names(rcp85.z)[1]<-"Valor"
+rcp45.p<-data.frame(extract(mk_pvalue,anp_coor), Variable="Pvalue")
+names(rcp45.p)[1]<-"Valor"
+rcp45.tau<-data.frame(extract(mk_tau,anp_coor), Variable="tau")
+names(rcp45.tau)[1]<-"Valor"
+rcp45.z<-data.frame(extract(mk_z,anp_coor), Variable="z")
+names(rcp45.z)[1]<-"Valor"
 
-rcp85<-rbind(rcp85.tau, rcp85.p,rcp85.z)
-names(rcp85)
-head(rcp85)
+rcp45<-rbind(rcp45.tau, rcp45.p,rcp45.z)
+names(rcp45)
+head(rcp45)
 
-base.df<-cbind(rcp85, anps_todas)
+base.df<-cbind(rcp45, anps_todas)
 head(base.df)
 
 tb_vars <- base.df %>% split(.$Variable) %>%  
-  map(~ group_by(.x,.$NOMBRE) %>% summarise(mu=mean(complete.cases(Valor)), 
-                                            var= var(complete.cases(Valor)),
-                                            area =unique(Area_km),
-                                            area_log = log(unique(Area_km)))) %>%
-  map(~na.omit(.x)) %>% map(~ mutate(.x,variable_min=mu-var,
-                                     variable_max=mu+var,ID_anp=1:length(mu)))
+  map(~ group_by(.x,.$NOMBRE) %>% summarise(Promedio=mean(Valor), 
+                                            var= var(Valor),
+                                            Área =unique(Area_km),
+                                            Área_log = log(unique(Area_km)))) %>%
+  map(~na.omit(.x)) %>% map(~ mutate(.x,variable_min=Promedio-var,
+                                     variable_max=Promedio+var,ID_anp=1:length(Promedio)))
 
-write.csv(tb_vars,"tminimahadgem2_es_all_rcp85.csv")
+write.csv(tb_vars,"tminimahadgem2_es_all_rcp45.csv")
 
-hadgem2_es_rcp85.vars<-data.frame(read.csv("tminimahadgem2_es_all_rcp85.csv"), MGC="HADGEM2_es_rcp85")
-names(hadgem2_es_rcp85.vars)
+HADGEM2_es_rcp45.vars<-data.frame(read.csv("tminimahadgem2_es_all_rcp45.csv"), MGC="HADGEM2_ES RCP 4.5")
+names(HADGEM2_es_rcp45.vars)
 
 #####mpi_esm_lr_####
-mk_pvalue<-raster("data_mk/tminimampi_esm_lr_rcp85_mk_p.tif")
+mk_pvalue<-raster("data_mk/tminimampi_esm_lr_rcp45_mk_p.tif")
 names(mk_pvalue)<-"mk_p"
-mk_tau<-raster("data_mk/tminimampi_esm_lr_rcp85_mk_tau.tif")
+mk_tau<-raster("data_mk/tminimampi_esm_lr_rcp45_mk_tau.tif")
 names(mk_tau)<-"mk_tau"
-mk_z<-raster("data_mk/tminimampi_esm_lr_rcp85_mk_Z.tif")
+mk_z<-raster("data_mk/tminimampi_esm_lr_rcp45_mk_Z.tif")
 names(mk_z)<-"mk_z"
 
-rcp85.p<-data.frame(extract(mk_pvalue,anp_coor), Variable="Pvalue")
-names(rcp85.p)[1]<-"Valor"
-rcp85.tau<-data.frame(extract(mk_tau,anp_coor), Variable="tau")
-names(rcp85.tau)[1]<-"Valor"
-rcp85.z<-data.frame(extract(mk_z,anp_coor), Variable="z")
-names(rcp85.z)[1]<-"Valor"
+rcp45.p<-data.frame(extract(mk_pvalue,anp_coor), Variable="Pvalue")
+names(rcp45.p)[1]<-"Valor"
+rcp45.tau<-data.frame(extract(mk_tau,anp_coor), Variable="tau")
+names(rcp45.tau)[1]<-"Valor"
+rcp45.z<-data.frame(extract(mk_z,anp_coor), Variable="z")
+names(rcp45.z)[1]<-"Valor"
 
-rcp85<-rbind(rcp85.tau, rcp85.p,rcp85.z)
-names(rcp85)
-head(rcp85)
+rcp45<-rbind(rcp45.tau, rcp45.p,rcp45.z)
+names(rcp45)
+head(rcp45)
 
-base.df<-cbind(rcp85, anps_todas)
+base.df<-cbind(rcp45, anps_todas)
 head(base.df)
 
 tb_vars <- base.df %>% split(.$Variable) %>%  
-  map(~ group_by(.x,.$NOMBRE) %>% summarise(mu=mean(complete.cases(Valor)), 
-                                            var= var(complete.cases(Valor)),
-                                            area =unique(Area_km),
-                                            area_log = log(unique(Area_km)))) %>%
-  map(~na.omit(.x)) %>% map(~ mutate(.x,variable_min=mu-var,
-                                     variable_max=mu+var,ID_anp=1:length(mu)))
+  map(~ group_by(.x,.$NOMBRE) %>% summarise(Promedio=mean(Valor), 
+                                            var= var(Valor),
+                                            Área =unique(Area_km),
+                                            Área_log = log(unique(Area_km)))) %>%
+  map(~na.omit(.x)) %>% map(~ mutate(.x,variable_min=Promedio-var,
+                                     variable_max=Promedio+var,ID_anp=1:length(Promedio)))
 
-write.csv(tb_vars,"tminimampi_esm_lr_all_rcp85.csv")
-mpi_esm_lr_rcp85.vars<-data.frame(read.csv("tminimampi_esm_lr_all_rcp85.csv"), MGC="MPI_ESM_LR_rcp85")
-names(mpi_esm_lr_rcp85.vars)
+write.csv(tb_vars,"tminimampi_esm_lr_all_rcp45.csv")
+MPI_ESM_LR_rcp45.vars<-data.frame(read.csv("tminimampi_esm_lr_all_rcp45.csv"), MGC="MPI_ESM_LR RCP 4.5")
+names(MPI_ESM_LR_rcp45.vars)
 
 #####gfdl_cm3_####
-mk_pvalue<-raster("data_mk/tminimagfdl_cm3_rcp85_mk_p.tif")
+mk_pvalue<-raster("data_mk/tminimagfdl_cm3_rcp45_mk_p.tif")
 names(mk_pvalue)<-"mk_p"
-mk_tau<-raster("data_mk/tminimagfdl_cm3_rcp85_mk_tau.tif")
+mk_tau<-raster("data_mk/tminimagfdl_cm3_rcp45_mk_tau.tif")
 names(mk_tau)<-"mk_tau"
-mk_z<-raster("data_mk/tminimagfdl_cm3_rcp85_mk_Z.tif")
+mk_z<-raster("data_mk/tminimagfdl_cm3_rcp45_mk_Z.tif")
 names(mk_z)<-"mk_z"
 
-rcp85.p<-data.frame(extract(mk_pvalue,anp_coor), Variable="Pvalue")
-names(rcp85.p)[1]<-"Valor"
-rcp85.tau<-data.frame(extract(mk_tau,anp_coor), Variable="tau")
-names(rcp85.tau)[1]<-"Valor"
-rcp85.z<-data.frame(extract(mk_z,anp_coor), Variable="z")
-names(rcp85.z)[1]<-"Valor"
+rcp45.p<-data.frame(extract(mk_pvalue,anp_coor), Variable="Pvalue")
+names(rcp45.p)[1]<-"Valor"
+rcp45.tau<-data.frame(extract(mk_tau,anp_coor), Variable="tau")
+names(rcp45.tau)[1]<-"Valor"
+rcp45.z<-data.frame(extract(mk_z,anp_coor), Variable="z")
+names(rcp45.z)[1]<-"Valor"
 
-rcp85<-rbind(rcp85.tau, rcp85.p,rcp85.z)
-names(rcp85)
-head(rcp85)
+rcp45<-rbind(rcp45.tau, rcp45.p,rcp45.z)
+names(rcp45)
+head(rcp45)
 
-base.df<-cbind(rcp85, anps_todas)
+base.df<-cbind(rcp45, anps_todas)
 head(base.df)
 
 tb_vars <- base.df %>% split(.$Variable) %>%  
-  map(~ group_by(.x,.$NOMBRE) %>% summarise(mu=mean(complete.cases(Valor)), 
-                                            var= var(complete.cases(Valor)),
-                                            area =unique(Area_km),
-                                            area_log = log(unique(Area_km)))) %>%
-  map(~na.omit(.x)) %>% map(~ mutate(.x,variable_min=mu-var,
-                                     variable_max=mu+var,ID_anp=1:length(mu)))
+  map(~ group_by(.x,.$NOMBRE) %>% summarise(Promedio=mean(Valor), 
+                                            var= var(Valor),
+                                            Área =unique(Area_km),
+                                            Área_log = log(unique(Area_km)))) %>%
+  map(~na.omit(.x)) %>% map(~ mutate(.x,variable_min=Promedio-var,
+                                     variable_max=Promedio+var,ID_anp=1:length(Promedio)))
 
-write.csv(tb_vars,"tminimagfdl_cm3_all_rcp85.csv")
-gfdl_cm3_rcp85.vars<-data.frame(read.csv("tminimagfdl_cm3_all_rcp85.csv"), MGC="GDFL_CM3_rcp85")
-names(gfdl_cm3_rcp85.vars)
+write.csv(tb_vars,"tminimagfdl_cm3_all_rcp45.csv")
+GDFL_CM3_rcp45.vars<-data.frame(read.csv("tminimagfdl_cm3_all_rcp45.csv"), MGC="GDFL_CM3 RCP 4.5")
+names(GDFL_CM3_rcp45.vars)
 
 ####bind####
-tb_vars<-rbind(CNRMCM5_rcp85.vars,gfdl_cm3_rcp85.vars,hadgem2_es_rcp85.vars,mpi_esm_lr_rcp85.vars)
-write.csv(tb_vars,"tminima_all_rcp85.csv")
-
+base<-rbind(CNRMCM5_rcp45.vars,GDFL_CM3_rcp45.vars,HADGEM2_es_rcp45.vars,MPI_ESM_LR_rcp45.vars)
+write.csv(base,"tminima_all_rcp45.csv")
--- Plotgrafica_MK.R
+++ Plotgrafica_MK.R
@@ -3,8 +3,8 @@ library(ggrepel)
 library(stringr)
 library(dplyr)
 ####
-#tb_vars %>% purrr::map(~ ggplot(.x, aes(x = area_log, y = mu,label =ID_anp )) +
- #                        geom_point(size = 1) +  geom_point(color="purple")+
+#tb_vars %>% purrr::map(~ ggplot(.x, aes(x = area_log, y = Promedio,label =ID_anp )) +
+ #                        geom_point(size = 1) +  geom_point(color="#2ca25f")+
   #                       geom_text_repel(aes(label=ID_anp), size = 3)) 
 setwd("~/Google Drive File Stream/Mi unidad/MannKendall")
 base<-read.csv("~/Google Drive File Stream/Mi unidad/MannKendall/TMinima_all_rcp45.csv")
@@ -25,43 +25,43 @@ CAD_1<-subset(base,str_detect(base$NOMBRE,"C.A.D.N.R. 004 Don Mart"))   #8   "C.
 M_1<-subset(base, str_detect(base$NOMBRE, "Mapim"))  #10  "Mapim?"
 
 tb_vars<-rbind(PC_1,LT_1,TC_1,MM_1, MN_1, J_1, SSPM_1,C1857_1, CS_1,SO_1, EV_1,CAD_1, M_1)
-Pvalue.05<-filter(tb_vars, Pvalue.mu <= 0.05)
-df0.05_0.7<-filter(Pvalue.05, tau.mu >= 0.8)
+Pvalue.05<-filter(tb_vars, Pvalue.Promedio <= 0.05)
+df0.05_0.7<-filter(Pvalue.05, tau.Promedio >= 0.8)
 
 ###tau_area_log####
-ggplot(tb_vars, aes(x = tau.area_log, y = tau.mu,label =z.ID_anp )) +
-  geom_point(size = 1) +  geom_point(color="purple")+
-  geom_text_repel(aes(label=z.ID_anp), size = 3)+
+ggplot(tb_vars, aes(x = tau.Área_log, y = tau.Promedio,label =z.ID_anp )) +
+  geom_point(size = 1) +  geom_point(color="#006d2c")+
+  geom_text_repel(aes(label=NOMBRE), size = 3)+
   facet_grid(MGC~.) +
-  geom_errorbar(aes(ymin = tau.variable_min, ymax = tau.variable_max), color="purple")
+  geom_errorbar(aes(ymin = tau.variable_min, ymax = tau.variable_max), color="#006d2c")
   #geom_errorbarh(aes(xmax = Pvalue.variable_max, xmin = Pvalue.variable_min))
-ggsave("tmin_graficas/tb_vars.jpg", dpi = 300)
+ggsave("tmin_graficas/Tau_rcp85.jpg", dpi = 300)
 
-ggplot(Pvalue.05, aes(x = tau.area_log, y = tau.mu,label =z.ID_anp )) +
-  geom_point(size = 1) +  geom_point(color="purple")+
-  geom_text_repel(aes(label=z.ID_anp), size = 3)+
+ggplot(Pvalue.05, aes(x = tau.Área_log, y = tau.Promedio,label =z.ID_anp )) +
+  geom_point(size = 1) +  geom_point(color="#41ae76")+
+  geom_text_repel(aes(label=NOMBRE), size = 3)+
   facet_grid(MGC~.) +
-  geom_errorbar(aes(ymin = tau.variable_min, ymax = tau.variable_max), color="purple")
+  geom_errorbar(aes(ymin = tau.variable_min, ymax = tau.variable_max), color="#41ae76")
 #geom_errorbarh(aes(xmax = Pvalue.variable_max, xmin = Pvalue.variable_min))
 ggsave("tmin_graficas/Pvalue.05_RCP85.jpg", dpi = 300)
 
-ggplot(df0.05_0.7, aes(x = tau.area_log, y = tau.mu,label =z.ID_anp )) +
-  geom_point(size = 1) +  geom_point(color="purple")+
+ggplot(df0.05_0.7, aes(x = tau.area_log, y = tau.Promedio,label =z.ID_anp )) +
+  geom_point(size = 1) +  geom_point(color="#2ca25f")+
   geom_text_repel(aes(label=z.ID_anp), size = 3)+
   facet_grid(MGC~.)+ 
-  geom_errorbar(aes(ymin = tau.variable_min, ymax = tau.variable_max),color="purple")
+  geom_errorbar(aes(ymin = tau.variable_min, ymax = tau.variable_max),color="#2ca25f")
   #geom_errorbarh(aes(xmax = Pvalue.variable_max, xmin = Pvalue.variable_min))
 
 ###tau_pvalue####
-ggplot(tb_vars, aes(x = Pvalue.mu, y = tau.mu,label =z.ID_anp )) +
-  geom_point(size = 1) +  geom_point(color="purple")+
+ggplot(tb_vars, aes(x = Pvalue.Promedio, y = tau.Promedio,label =z.ID_anp )) +
+  geom_point(size = 1) +  geom_point(color="#2ca25f")+
   #geom_text_repel(aes(label=z.ID_anp), size = 3)+
   facet_grid(MGC~.) +
   geom_errorbar(aes(ymin = tau.variable_min, ymax = tau.variable_max))+
   geom_errorbarh(aes(xmax = Pvalue.variable_max, xmin = Pvalue.variable_min))
 
-ggplot(df0.05_0.7, aes(x = Pvalue.mu, y = tau.mu,label =z.ID_anp )) +
-  geom_point(size = 1) +  geom_point(color="purple")+
+ggplot(df0.05_0.7, aes(x = Pvalue.Promedio, y = tau.Promedio,label =z.ID_anp )) +
+  geom_point(size = 1) +  geom_point(color="#2ca25f")+
   #geom_text_repel(aes(label=z.ID_anp), size = 3)+
   facet_grid(MGC~.)+ 
   geom_errorbar(aes(ymin = tau.variable_min, ymax = tau.variable_max))+
